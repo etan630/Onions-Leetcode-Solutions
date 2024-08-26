1e134719b4e5204ff7f6c1e200a5c354e2ec1391
etan630 <79673596+etan630@users.noreply.github.com>
2024-08-26 16:18:30 -0400

    Update IOI '94 P1 - The Triangle.py

--- IOI '94 P1 - The Triangle.py
+++ IOI '94 P1 - The Triangle.py
@@ -1,1 +1,19 @@
+#triangle[i][j]  reps max path sum from top of traingle to element at pos i, j
+# triangle i j <= triangle i j + max(triangle i + 1, j ; triangle [i + 2, j + 1])
+#       start w curr val then take below left and below right to find max
+# base case: last row of triangle
+def max_sum(triangle, N):
+    for i in range(N - 2, -1, -1):
+        for j in range(i + 1):
+            triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j+1])
+            
+    return triangle[0][0]
 
+# reading inputs
+N = int(input().strip())
+triangle = []
+for _ in range(N):
+    row = list(map(int, input().strip().split()))
+    triangle.append(row)
+    
+print(max_sum(triangle, N))
